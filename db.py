053e48d743aa4b94daba9e9267154b68c6a0cdc9
shrinish123 <shrinishiitkgp2001@gmail.com>
2025-08-17 16:45:44 +0530

    add env varaibles

--- db.py
+++ db.py
@@ -1,8 +1,11 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://echoforms:password@localhost:5432/echoforms")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DB_CONNECTION_STRING")
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
